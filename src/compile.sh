7ba32f7eed6876a8754520a8f0194fe15cf8ed13
James Butcher <jamesbutcher167@gmail.com>
2022-06-02 14:32:59 +0100

    Add basic 3D

--- src/compile.sh
+++ src/compile.sh
@@ -1,1 +1,3 @@
+#/bin/sh
+
 clang main.c -lraylib -ldl -pthread -lGL -lm
--- src/main.c
+++ src/main.c
@@ -1,3 +1,62 @@
 #include <raylib.h>
+#include <stdlib.h>
 
-int main(){return 0;}
+#define WIDTH 800
+#define HEIGHT 450
+
+typedef struct Player {
+    Vector3* position;
+    Vector3 size;
+    Camera* camera;
+} Player;
+
+Player* InitPlayer(Vector3 position, Vector3 size) {
+    Player* allocation = (Player*)malloc(sizeof(Player));
+
+    *allocation->camera = (Camera3D){
+        position,
+        (Vector3){ 1.0f, 0.0f, 0.0f },
+        (Vector3){ 0.0f, 1.0f, 0.0f },
+        45.f,
+        CAMERA_PERSPECTIVE,
+    };
+
+    allocation->position = &allocation->camera->position;
+    allocation->size = size;
+
+    SetCameraMode(*allocation->camera, CAMERA_FIRST_PERSON);
+
+    return allocation;
+}
+
+void UpdatePlayer(Player* player) {
+    UpdateCamera(player->camera);
+}
+
+int main(){
+    InitWindow(WIDTH, HEIGHT, "Haemophilia");
+
+    Player* player = InitPlayer((Vector3){0.f, 0.f, 0.f}, (Vector3){2.f, 2.f, 2.f});
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+
+        UpdatePlayer(player);
+
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        BeginMode3D(*player->camera);
+        DrawCubeV((Vector3){2.f, 0.f, 0.f}, (Vector3){2.f, 2.f, 2.f}, RED);
+
+        EndMode3D();
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
