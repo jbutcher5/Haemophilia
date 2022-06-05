ff65144ded112aeab34ff84e71bc7adfead35a79
James Butcher <jamesbutcher167@gmail.com>
2022-06-05 17:31:13 +0100

    Fix camera rotation by setting the camera target extremely far away

--- src/main.c
+++ src/main.c
@@ -2,9 +2,12 @@
 #include <raymath.h>
 #include <math.h>
 #include <stdlib.h>
+#include <float.h>
 
 #define WIDTH 960
 #define HIEGHT 540
+
+#define MODULO(a, n) fmod((a), (n)) + (((a) < 0) * (n))
 
 typedef struct Player {
     Vector3* position;
@@ -17,8 +20,8 @@ typedef struct Player {
 Vector3 rotateVectorY(const Vector3 v, const float theta) {
     Vector3 result = v;
 
-    result.x = cosf(theta) * v.x - sinf(theta) * v.y;
-    result.y = sinf(theta) * v.x + cosf(theta) * v.y;
+    result.x = cosf(-theta) * v.x - sinf(-theta) * v.y;
+    result.y = sinf(-theta) * v.x + cosf(-theta) * v.y;
 
     return result;
 }
@@ -26,8 +29,8 @@ Vector3 rotateVectorY(const Vector3 v, const float theta) {
 Vector3 rotateVectorZ(const Vector3 v, const float theta) {
     Vector3 result = v;
 
-    result.z = -sinf(theta) * v.x + cosf(theta) * v.z;
-    result.x = cosf(theta) * v.x + sinf(theta) * v.z;
+    result.z = -sinf(-theta) * v.x + cosf(-theta) * v.z;
+    result.x = cosf(-theta) * v.x + sinf(-theta) * v.z;
 
     return result;
 }
@@ -37,9 +40,9 @@ Player* InitPlayer(const Vector3 position, const Vector3 size) {
 
     allocation->camera = (Camera3D){
         position,
-        (Vector3){ 1.0f, 0.0f, 0.0f },
-        (Vector3){ 0.0f, 1.0f, 0.0f },
-        80.f,
+        (Vector3){1073741824.f, 0.0f, 0.0f},
+        (Vector3){0.0f, 1.f, 0.0f},
+        60.f,
         CAMERA_PERSPECTIVE,
     };
 
@@ -54,14 +57,36 @@ Player* InitPlayer(const Vector3 position, const Vector3 size) {
 
 void UpdatePlayer(Player* player) {
     Vector2 mouseDelta = GetMouseDelta();
-    float deltaY = -mouseDelta.y/75;
-    float deltaZ = -mouseDelta.x/75;
 
-    player->thetaY += deltaY;
-    player->thetaZ += deltaZ;
+    float deltaY = mouseDelta.y/75;
+    float deltaZ = mouseDelta.x/75;
 
-    player->camera.target = rotateVectorY(player->camera.target, deltaY);
+    float totalY = MODULO(player->thetaY + deltaY, M_PI);
+    player->thetaZ = MODULO(player->thetaZ + deltaZ, 2*M_PI);
     player->camera.target = rotateVectorZ(player->camera.target, deltaZ);
+    player->camera.target = rotateVectorY(player->camera.target, deltaY);
+    player->thetaY = totalY;
+
+    if (IsKeyDown(KEY_W)) {
+        player->position->x += .25f;
+        player->camera.target.x += .25f;
+    }
+
+    if (IsKeyDown(KEY_S)) {
+        player->position->x -= .25f;
+        player->camera.target.x -= .25f;
+    }
+
+    if (IsKeyDown(KEY_A)) {
+        player->position->z -= .25f;
+        player->camera.target.z -= .25f;
+    }
+
+    if (IsKeyDown(KEY_D)) {
+       player->position->z += .25f;
+       player->camera.target.z += .25f;
+    }
+
 }
 
 int main(){
@@ -82,6 +107,7 @@ int main(){
 
         BeginMode3D(player->camera);
         DrawCubeV((Vector3){2.f, 0.f, 0.f}, (Vector3){2.f, 2.f, 2.f}, RED);
+        DrawCubeV((Vector3){0.f, 0.f, 4.f}, (Vector3){2.f, 3.f, 2.f}, BLUE);
 
         EndMode3D();
 
