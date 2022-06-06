f1dfa2ac18cac4a5822af4140894568778a8b16b
James Butcher <jamesbutcher167@gmail.com>
2022-06-06 19:58:52 +0100

    Add custom camera

--- src/main.c
+++ src/main.c
@@ -1,10 +1,12 @@
 #include <raylib.h>
 #include <raymath.h>
+#include <rlgl.h>
 #include <math.h>
 #include <stdlib.h>
 
 #define WIDTH 960
-#define HIEGHT 540
+#define HEIGHT 540
+#define RL_PROJECTION 0x1701
 
 #define MODULO(a, n) fmod((a), (n)) + (((a) < 0) * (n))
 
@@ -60,42 +62,29 @@ void UpdatePlayer() {
     }
 }
 
-void UpdatePlayer(Player* player) {
-    Vector2 mouseDelta = GetMouseDelta();
+void StartDisplay() {
+    rlDrawRenderBatchActive();
+    rlMatrixMode(RL_PROJECTION);
+    rlPushMatrix();
+    rlLoadIdentity();
 
-    float deltaY = mouseDelta.y/75;
-    float deltaZ = mouseDelta.x/75;
+    float aspect = (float)WIDTH/(float)HEIGHT;
 
-    float totalY = MODULO(player->thetaY + deltaY, M_PI);
-    player->thetaZ = MODULO(player->thetaZ + deltaZ, 2*M_PI);
-    player->camera.target = rotateVectorZ(player->camera.target, deltaZ);
-    player->camera.target = rotateVectorY(player->camera.target, deltaY);
-    player->thetaY = totalY;
+    double top = RL_CULL_DISTANCE_NEAR*tan(80*.4*DEG2RAD);
+    double right = top*aspect;
 
-    if (IsKeyDown(KEY_W)) {
-        player->position->x += .25f;
-        player->camera.target.x += .25f;
-    }
+    rlFrustum(-right, right, -top, top, RL_CULL_DISTANCE_NEAR, RL_CULL_DISTANCE_FAR);
+    rlMatrixMode(RL_MODELVIEW);
+    rlLoadIdentity();
 
-    if (IsKeyDown(KEY_S)) {
-        player->position->x -= .25f;
-        player->camera.target.x -= .25f;
-    }
+    Matrix matView = MatrixLookAt(player.position, player.target, (Vector3){0.f, 1.f, 0.f});
+    rlMultMatrixf(MatrixToFloat(matView));
 
-    if (IsKeyDown(KEY_A)) {
-        player->position->z -= .25f;
-        player->camera.target.z -= .25f;
-    }
-
-    if (IsKeyDown(KEY_D)) {
-       player->position->z += .25f;
-       player->camera.target.z += .25f;
-    }
-
+    rlEnableDepthTest();
 }
 
 int main(){
-    InitWindow(WIDTH, HIEGHT, "Haemophilia");
+    InitWindow(WIDTH, HEIGHT, "Haemophilia");
     DisableCursor();
 
     SetTargetFPS(60);
@@ -108,7 +97,8 @@ int main(){
 
         ClearBackground(RAYWHITE);
 
-        BeginMode3D(player->camera);
+        StartDisplay();
+
         DrawCubeV((Vector3){2.f, 0.f, 0.f}, (Vector3){2.f, 2.f, 2.f}, RED);
         DrawCubeV((Vector3){0.f, 0.f, 4.f}, (Vector3){2.f, 3.f, 2.f}, BLUE);
 
