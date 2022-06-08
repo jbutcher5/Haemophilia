1bc43e71e0f9f3df6114537d39c61afa5fc426cc
James Butcher <jamesbutcher167@gmail.com>
2022-06-08 18:01:48 +0100

    Add other directions of movement

--- src/main.c
+++ src/main.c
@@ -57,11 +57,24 @@ void UpdatePlayer() {
     player.target = rotateVectorVertical(player.target, -delta.y);
     player.target = rotateVectorHorizontal(player.target, delta.x);
 
-    Vector2 direction = (Vector2){cosf(player.theta.x)*.25f, sinf(player.theta.x)*.25f};
+    Vector3 direction = (Vector3){cosf(player.theta.x)*.25f, 0.f, sinf(player.theta.x)*.25f};
 
     if (IsKeyDown(KEY_W)) {
-        player.position.x += direction.x;
-        player.position.z += direction.y;
+        player.position = Vector3Add(player.position, direction);
+    }
+
+    if (IsKeyDown(KEY_D)) {
+        Vector3 newDirection = rotateVectorHorizontal(direction, M_PI/2);
+        player.position = Vector3Add(player.position, newDirection);
+    }
+
+    if (IsKeyDown(KEY_A)) {
+        Vector3 newDirection = rotateVectorHorizontal(direction, 3*(M_PI/2));
+        player.position = Vector3Add(player.position, newDirection);
+    }
+
+    if (IsKeyDown(KEY_S)) {
+        player.position = Vector3Subtract(player.position, direction);
     }
 }
 
