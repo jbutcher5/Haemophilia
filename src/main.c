9d1fafb4eba5fdbee6a6c856673d743015d6ac40
James Butcher <jamesbutcher167@gmail.com>
2022-06-13 14:52:02 +0100

    Add object to object and point to object collision detection

--- src/main.c
+++ src/main.c
@@ -53,6 +53,26 @@ bool isPlayerColliding(const Object object) {
         (playerOrigin.z <= objectMax.z && playerMax.z >= objectOrigin.z);
 }
 
+bool isObjectColliding(const Object a, const Object b) {
+    Vector3 aOrigin = boundingBoxOrigin(a.position, a.size);
+    Vector3 bOrigin = boundingBoxOrigin(b.position, b.size);
+    Vector3 aMax = boundingBoxMax(a.position, a.size);
+    Vector3 bMax = boundingBoxMax(b.position, b.size);
+
+    return (aOrigin.x <= bMax.x && aMax.x >= bOrigin.x) &&
+        (aOrigin.y <= bMax.y && aMax.y >= bOrigin.y) &&
+        (aOrigin.z <= bMax.z && aMax.z >= bOrigin.z);
+}
+
+bool isPointColliding(const Vector3 point, const Object object) {
+    Vector3 objectOrigin = boundingBoxOrigin(object.position, object.size);
+    Vector3 objectMax = boundingBoxMax(object.position, object.size);
+
+    return (point.x >= objectOrigin.x && point.x <= objectMax.x) &&
+        (point.y >= objectOrigin.y && point.y <= objectMax.y) &&
+        (point.z >= objectOrigin.z && point.z <= objectMax.z);
+}
+
 Vector3 rotateVectorVertical(const Vector3 v, const float theta) {
     Vector3 result = v;
 
