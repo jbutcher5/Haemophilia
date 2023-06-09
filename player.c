90fbf2c00accdb7bba6f24b35b0b016d87f35e2e
James Butcher <jamesbutcher167@gmail.com>
2023-06-09 23:28:58 +0000

    Move all UpdatePosition operations into procedures

--- player.c
+++ player.c
@@ -7,12 +7,7 @@
 #include "more_math.h"
 #include "player.h"
 
-void UpdatePlayer(Player *player, BoundingBox *objects, int n) {
-    Vector3 player_velocity = Vector3Zero();
-    float time = GetTime();
-
-    // Mouse Movements
-
+void MouseMovements(Player *player) {
     Vector2 mouse_delta = GetMouseDelta();
     Vector2 delta = {mouse_delta.x / 100, -mouse_delta.y / 100};
 
@@ -22,9 +17,9 @@ void UpdatePlayer(Player *player, BoundingBox *objects, int n) {
     player->theta.x = atan2f(player->target.z, player->target.x);
 
     player->target = RotateVector3(player->target, delta);
+}
 
-    // Horizontal Movements
-
+void HorizontalMovement(Player *player, double time) {
     if (IsKeyPressed(KEY_SPACE) && !player->is_jumping && !player->is_falling) {
         player->is_jumping = true;
         player->started_jumping = time;
@@ -34,7 +29,9 @@ void UpdatePlayer(Player *player, BoundingBox *objects, int n) {
         player->is_running = true;
         player->started_running = time;
     }
+}
 
+void MovementCalc(Player *player, double time, Vector3 *player_velocity) {
     float velocity_multiplyer = 15.f;
 
     if (player->is_running)
@@ -51,22 +48,25 @@ void UpdatePlayer(Player *player, BoundingBox *objects, int n) {
     if (Vector3Length(cartesian)) {
         float rotation = atan2f(cartesian.z, cartesian.x);
         direction = RotateVector3(direction, (Vector2){rotation, 0.f});
-        player_velocity = Vector3Add(player_velocity, direction);
+        *player_velocity = Vector3Add(*player_velocity, direction);
     }
 
     // Update Position
 
     if (player->is_falling) {
-        player_velocity.y += FallingVelocity(time - player->started_falling);
+        player_velocity->y += FallingVelocity(time - player->started_falling);
     }
 
     if (player->is_jumping) {
         float jumping_delta = time - player->started_jumping;
 
         player->is_jumping = DoJumping(jumping_delta);
-        player_velocity.y += JumpingVelocity(jumping_delta);
+        player_velocity->y += JumpingVelocity(jumping_delta);
     }
+}
 
+void CollisionCalc(Player *player, double time, Vector3 *player_velocity,
+                   BoundingBox *objects, int n) {
     BoundingBox foot_collider = player->hitbox;
     UpdatePosition(&foot_collider, (Vector3){0.f, -SIZE.y / 2, 0.f});
     SetSize(&foot_collider, (Vector3){SIZE.x, 0.01f, SIZE.z});
@@ -78,24 +78,37 @@ void UpdatePlayer(Player *player, BoundingBox *objects, int n) {
     for (int i = 0; i < n; i++) {
         if (CheckCollisionBoxes(foot_collider, objects[i])) {
             player->is_falling = false;
-        } else if (!CheckCollisionBoxes(foot_collider, objects[i]) &&
-                   !player->is_jumping) {
+        } else if (CheckCollisionBoxes(head_collider, objects[i])) {
+            // UpdatePosition(&player->hitbox, (Vector3){0, -0.02, 0});
+            player->is_falling = player->is_falling;
+            player->started_falling = time;
+            player->is_jumping = false;
+        } else if (CheckCollisionBoxes(player->hitbox, objects[i])) {
+            *player_velocity = (Vector3){0.f, player_velocity->y, 0.f};
+        }
+
+        if (!CheckCollisionBoxes(foot_collider, objects[i]) &&
+            !player->is_jumping) {
             if (!player->is_falling) {
                 player->started_falling = time;
             }
 
             player->is_falling = true;
-        } else if (CheckCollisionBoxes(head_collider, objects[i])) {
-            player->is_falling = player->is_falling;
-            player->started_falling = time;
-            player->is_jumping = false;
-        } else if (CheckCollisionBoxes(player->hitbox, objects[i])) {
-            player_velocity = (Vector3){0.f, player_velocity.y, 0.f};
         }
     }
 
     UpdatePosition(&player->hitbox,
-                   Vector3Scale(player_velocity, GetFrameTime()));
+                   Vector3Scale(*player_velocity, GetFrameTime()));
+}
+
+void UpdatePlayer(Player *player, BoundingBox *objects, int n) {
+    double time = GetTime();
+    Vector3 player_velocity = Vector3Zero();
+
+    MouseMovements(player);
+    HorizontalMovement(player, time);
+    MovementCalc(player, time, &player_velocity);
+    CollisionCalc(player, time, &player_velocity, objects, n);
 }
 
 Ray GetPlayerRay(Player *player) {
